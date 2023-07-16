9ed038acd3e2f7ac27f0ee3096908e38cb4adbdd
Daniel de Almeida Bernardi <98421842+Danyel1112@users.noreply.github.com>
2023-07-16 15:02:36 -0300

    Atualização

--- OficinaAtos-main/src/main/java/com/springproject/oficinaatos/controller/PDFController.java
+++ OficinaAtos-main/src/main/java/com/springproject/oficinaatos/controller/PDFController.java
@@ -1,9 +1,13 @@
 package com.springproject.oficinaatos.controller;
 
+import com.itextpdf.text.pdf.PdfPCell;
 import com.springproject.oficinaatos.model.Cliente;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -20,12 +24,20 @@ public class PDFController {
             PdfWriter.getInstance(document, baos);
             document.open();
 
-            document.add(new Paragraph("Informações do Cliente"));
-            document.add(new Paragraph("Nome: " + cliente.getNome()));
-            document.add(new Paragraph("Telefone: " + cliente.getTelefone()));
-            document.add(new Paragraph("Email: " + cliente.getEmail()));
-            document.add(new Paragraph("Descrição: " + cliente.getDescricao()));
+            Font titleFont = new Font(Font.FontFamily.HELVETICA, 18, Font.BOLD);
+            Font contentFont = new Font(Font.FontFamily.HELVETICA, 12, Font.NORMAL);
 
+            addTitle(document, "Informações do Cliente", titleFont);
+
+            PdfPTable table = new PdfPTable(2);
+            table.setWidthPercentage(100);
+
+            addRowToTable(table, "Nome:", cliente.getNome(), contentFont);
+            addRowToTable(table, "Telefone:", cliente.getTelefone(), contentFont);
+            addRowToTable(table, "Email:", cliente.getEmail(), contentFont);
+            addRowToTable(table, "Descrição:", cliente.getDescricao(), contentFont);
+
+            document.add(table);
             document.close();
 
             byte[] contents = baos.toByteArray();
@@ -40,5 +52,23 @@ public class PDFController {
         }
 
         return ResponseEntity.badRequest().build();
+    }
+
+    private static void addTitle(Document document, String title, Font font) throws DocumentException {
+        Paragraph paragraph = new Paragraph(title, font);
+        paragraph.setAlignment(Element.ALIGN_CENTER);
+        paragraph.setSpacingAfter(20);
+        document.add(paragraph);
+    }
+
+    private static void addRowToTable(PdfPTable table, String label, String value, Font font) {
+        table.addCell(createCell(label, font));
+        table.addCell(createCell(value, font));
+    }
+
+    private static PdfPCell createCell(String content, Font font) {
+        PdfPCell cell = new PdfPCell(new Paragraph(content, font));
+        cell.setPadding(5);
+        return cell;
     }
 }
